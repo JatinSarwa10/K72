cc24abd2db5cce045eadac28ad006a6c8ae1c056
JatinSarwa10 <sarwajatin@gmail.com>
2025-09-11 14:08:00 +0530

    update video.jsx

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import Video from './Video'
 
-
-
 const HomeHeroText = () => {
-    return (
-        <div className='font-[font1]  mt-72 lg:mt-0 pt-5 text-center'>
-            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[10vw]'>
-                L'étincelle
-            </div>
-            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-start uppercase lg:leading-[8vw] leading-[10vw]'>
-                qui
-                <div className='h-[7vw] w-[14vw] rounded-full -mt-3 overflow-hidden'>
-                    <Video />
-                </div>
-                génère
-            </div>
-            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[10vw]'>
-                la créativité
-            </div>
-        </div>
-    )
+  return (
+    <div className="font-[font1] mt-72 lg:mt-0 pt-5 text-center">
+      {/* Line 1 */}
+      <div className="lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[10vw]">
+        The spark
+      </div>
+
+      {/* Line 2 with pill video */}
+      <div className="lg:text-[9.5vw] text-[12vw] justify-center flex items-start uppercase lg:leading-[8vw] leading-[10vw]">
+        Who
+        <div className="w-[14vw] h-[7vw] rounded-full  overflow-hidden   -mt-1 flex items-center justify-center mx-2">
+  <Video />
+</div>
+        generates
+      </div>
+
+      {/* Line 3 */}
+      <div className="lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[10vw]">
+        creativity
+      </div>
+    </div>
+  )
 }
 
 export default HomeHeroText
--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Video = () => {
   return (
-    <div className='h-full w-full '>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="K72Home.mp4"></video>
-    </div>
+    <video
+      className="w-full h-full object-fill rounded-xl "
+      autoPlay
+      loop
+      muted
+      playsInline
+      src="https://download-video-ak.vimeocdn.com/v3-1/playback/a278bb9c-996c-4760-8ca1-dd0abff308f7/83c745cf?__token__=st=1757579635~exp=1757583235~acl=%2Fv3-1%2Fplayback%2Fa278bb9c-996c-4760-8ca1-dd0abff308f7%2F83c745cf%2A~hmac=f194648937d09906ee225b4a8efbcad1d2e746ae69d1d5def9f28b615b7012f2&r=dXMtd2VzdDE%3D"
+    />
   )
 }
 
